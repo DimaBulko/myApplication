3d0c4c35014880b6633fff96d19370e0ba40c76e
Dima Bulko <dimabulkoff@gmail.com>
2023-02-20 20:51:21 +0300

    my first commit

---
+++ app/src/main/java/Game.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val name = "Madrigal"
+    val race = "gnome"
+    val healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal // true or false
+    val auraColor = if (auraVisible) "GREEN" else "NONE" // в зависимости от auraColor
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        } else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+    println(auraColor)
+    println(name + healthStatus)
+}
---
+++ app/src/main/java/TypePeremennye.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var hasSteed = 0
+    val playerName = "Estragon"
+    val nameCafe = "Рог единорога"
+    val nameBarista = "Mike"
+    var valueCoins = 50
+    val menuDrink = listOf("honey", "wine", "beer")
+    println(menuDrink)
+    val playerNameReverse = playerName.reversed()
+    println(playerNameReverse)
+    valueCoins -= 25
+    println(valueCoins)
+
+
+}
